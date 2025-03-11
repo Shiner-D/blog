f2b731cadfcca51d8b0b5de6889d3d32d416c512
Shiner-D <dongliang142857@gmail.com>
2025-03-11 11:55:41 +0800

    打包设置

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import path from "path";
 import { defineConfig } from "vite";
+import { visualizer } from "rollup-plugin-visualizer"
 import eslint from "vite-plugin-eslint";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react(), eslint()],
+  plugins: [react(), eslint(), visualizer()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -13,5 +14,18 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true,
+  },
+  build: {
+    minify: "terser",
+    sourcemap: true, // 生成 sourcemap 以便调试
+    rollupOptions: {
+      treeshake: true,
+      output: {
+        manualChunks: {
+          // 手动分块，避免代码重复
+          react: ["react", "react-dom"],
+        },
+      },
+    },
   },
 });
